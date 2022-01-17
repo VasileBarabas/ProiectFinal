12d4bcc05683ce931a557581064306684b09fb18
VasileBarabas <vasilebarabas@yahoo.com>
2022-01-17 13:08:02 +0200

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProiectFinal.Models;
+using System.Threading.Tasks;
+namespace ProiectFinal.Controllers
+{
+    public class UsersController : Controller
+    {
+        public string ErrorMessage;
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verify(User user)
+        {
+            var res = VerifyLogin(user);
+            if(res.Result)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "Contul introdus nu exista, reincercati";
+                return View("Login");
+            }
+         
+        }
+        public async Task<bool> VerifyLogin(User user)
+        {
+                using (UserDbContex udb = new UserDbContex())
+                {
+                    var res = udb.Users.SingleOrDefault(u => u.Password == user.Password && u.Username == user.Username);
+                    if (res != null)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddToDb(User user)
+        {
+            Console.WriteLine("Vream Sa bagam in db");
+
+            if (ModelState.IsValid)
+            {
+               var res= InsertToDb(user);
+                ViewBag.Message = ErrorMessage;
+                
+            }
+            return View("SignUp");
+        }
+      
+        public async Task InsertToDb(User user)
+        {
+ 
+                try
+                {
+                    using (UserDbContex udb = new UserDbContex())
+                    {
+
+                        var res = udb.Users.SingleOrDefault(u => u.Password == user.Password && u.Username == user.Username);
+                    if (res != null)
+                    {
+                        ErrorMessage = "Contul exista deja, reincercati";
+
+                    }
+                    else
+                    {
+
+                        udb.Users.Add(user);
+                        udb.SaveChanges();
+                        ErrorMessage = "Cont inserat cu succes";
+                    }
+                    }
+                }
+                catch(Exception e)
+                {
+                Console.WriteLine(e.Message);
+                }
+        }
+    }
+  
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace ProiectFinal.Models
+{
+    public class User
+    { 
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+    public class UserDbContex : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
